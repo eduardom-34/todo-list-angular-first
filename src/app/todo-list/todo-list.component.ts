ec4ec46f9cfca6c27ed62160be8d190a14fd6741
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-02 20:03:04 -0600

    Delete button

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+
+  taskArray = [ {taskName: 'Brush teeth', isCompleted: false} ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit( form: NgForm ) {
+
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+  }
+
+  onDelete( index: number ) {
+    console.log({index});
+
+    this.taskArray.splice(index, 1)
+
+  }
 
 }
